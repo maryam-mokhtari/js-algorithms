430c5d174f5a51a78c8e5fecd9af94c1c66968b9
Maryam Mokhtari <maryam.mokhtari@gmail.com>
2021-09-24 19:32:08 +0330

    Add Selection Sort

---
+++ sort/selectionSort.js
@@ -0,0 +1,14 @@
+const selectionSort = q => {
+    let n = q.length
+    for (let i = 0; i < n - 1; i++) {
+        minj = i
+        for(let j = i + 1;  j < n; j++) {
+            if (q[j] < q[minj]) {
+                minj = j
+            }
+        }
+        if (minj != i) {
+            swap(q, i, minj)
+        }
+    }
+}
