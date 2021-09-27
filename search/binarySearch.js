be7bb5721824dbc1c85a912605cbd815a3d7b103
Maryam Mokhtari <maryam.mokhtari@gmail.com>
2021-09-28 02:42:36 +0330

    Add repeated for Searchs

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -1,15 +1,46 @@
 // Array should be already sorted.
 
 const binarySearch = (q, key, low = 0, high = q.length) => {
+  if (low > high) {
+    return -1;
+  }
+  let mid = parseInt((low + high) / 2);
+  if (q[mid] < key) {
+    return binarySearch(q, key, mid + 1, high);
+  } else if (q[mid] > key) {
+    return binarySearch(q, key, low, mid - 1);
+  } else {
+    return mid;
+  }
+};
+
+// When the first occurrence of element is needed for repeated elemets:
+const binarySearch_u_l = (q, key, low = 0, high = q.length) => {
+  if (low > high) {
+    return -1
+  }
+  let mid = parseInt((low + high) / 2)
+  if (q[mid] < key) {
+    return binarySearch_u_l(q, key, mid + 1, high)
+  } else if (q[mid] > key || (q[mid] === key && mid > 0 && q[mid - 1] === key)) {
+    return binarySearch_u_l(q, key, low, mid - 1)
+  } else {
+    return mid
+  }
+}
+
+// When the last occurrence of element is needed for repeated elemets:
+const binarySearch_u_r = (q, key, low = 0, high = q.length) => {
     if (low > high) {
-        return -1
+      return -1
     }
     let mid = parseInt((low + high) / 2)
-    if (q[mid] < key) {
-        return binarySearch(q, key, mid + 1, high)
+    if (q[mid] < key || (q[mid] === key && mid < q.length && q[mid + 1] === key)) {
+      return binarySearch_u_r(q, key, mid + 1, high)
     } else if (q[mid] > key) {
-        return binarySearch(q, key, low, mid - 1)
+      return binarySearch_u_r(q, key, low, mid - 1)
     } else {
-        return mid
+      return mid
     }
-}
+  }
+  
--- search/linearSearch.js
+++ search/linearSearch.js
@@ -9,3 +9,14 @@ const linearSearch = (q, key) => {
   }
   return -1
 }
+
+// If the last occurrence of the key is needed when repeated elements occurs:
+const linearSearch_u = (q, key) => {
+  let n = q.length
+  for (let i = 0; i < n; i++) {
+    if (q[i] === key && (q[i] !== q[i + 1] || i == n - 1)) {
+      return i
+    }
+  }
+  return -1
+}
