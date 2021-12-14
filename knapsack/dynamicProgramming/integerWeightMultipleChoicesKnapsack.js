9bc3efdcd9060db637682a9a1061dd403496b3d4
Maryam Mokhtari <maryam.mokhtari@gmail.com>
2021-12-14 18:24:45 +0330

    Update and rename integerWeightMultipleChoiceKnapsack.js to integerWeightMultipleChoicesKnapsack.js

---
+++ knapsack/dynamicProgramming/integerWeightMultipleChoicesKnapsack.js
@@ -0,0 +1,41 @@
+const knapsack = (items, w) => {
+  let choices = {}
+  let values = []
+
+  const callKnapsack = (items, w) => {
+    const n = items.length
+    for (let i = 0; i <= w; i++) {
+      let maxIndex = 0
+      let max = 0
+      for (let j = 0; j < n; j++) {
+        if (i >= items[j].weight) {
+          const value = items[j].value + values[i - items[j].weight]
+          if (value > max) {
+            max = value
+            maxIndex = j
+          }
+        }
+      }
+      values[i] = max
+      choices[i] = maxIndex
+    }
+  }
+
+  const print = (items, w) => {
+    console.log('Max Value:', values[w])
+    while (w) {
+      console.log(`${choices[w]}: {w: ${items[choices[w]].weight}, v: ${items[choices[w]].value}}`)
+      w = w - items[choices[w]].weight
+    }
+  }
+
+  const printK = (items, w) => {
+    console.log(values[w])
+    if (w <= 0) return ''
+    return choices[w] + ':' + printK(items, w - items[choices[w]].weight)
+  }
+
+  callKnapsack(items, w)
+  print(items, w)
+  //console.log(printK(items, w))
+}
